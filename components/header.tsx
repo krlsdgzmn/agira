ef6638099905242a8f254e23f342c4aeeddb4db8
krlsdgzmn <johnkarlos.deguzman.d@gmail.com>
2024-07-21 16:23:08 +0800

    🔧 Added user button

--- components/header.tsx
+++ components/header.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { UserButton, useUser } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
@@ -7,6 +8,7 @@ import { Button } from "./ui/button";
 
 export default function Header() {
   const path = usePathname();
+  const user = useUser();
 
   return (
     <header className="top-0 z-50 w-full border-b border-border bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 md:sticky">
@@ -19,25 +21,31 @@ export default function Header() {
             className="h-10 w-10"
             width={10}
             height={10}
-            priority
           />
           <h1 className="text-lg font-bold text-farm">Agira</h1>
         </Link>
 
         {/* add nav here */}
-        <div className="flex items-center gap-2">
-          {path !== "/sign-in" && (
+        <div className="flex items-center space-x-2">
+          {path !== "/sign-in" && !user.isSignedIn && (
             <Link href="/sign-in">
               <Button variant="outline">Login</Button>
             </Link>
           )}
 
-          {path !== "/sign-up" && (
+          {path !== "/sign-up" && !user.isSignedIn && (
             <Link href="/sign-up">
               <Button className="bg-farm text-white hover:bg-farm/90">
                 Get Started
               </Button>
             </Link>
+          )}
+
+          {/* Mount the UserButton component */}
+          {user.isSignedIn && (
+            <div className="flex h-10 items-end justify-center pb-1">
+              <UserButton showName />
+            </div>
           )}
         </div>
       </div>
