6412ac3f265c8fa8f78df6a36fca73e396a1d683
krlsdgzmn <johnkarlos.deguzman.d@gmail.com>
2024-07-24 18:46:40 +0800

    🔧 Added navigation links

--- components/header.tsx
+++ components/header.tsx
@@ -6,29 +6,47 @@ import Link from "next/link";
 import SearchInput from "./search-input";
 import { Button } from "./ui/button";
 
+const links = [
+  { name: "Marketplace", href: "/marketplace" },
+  { name: "About", href: "#" },
+  { name: "Terms & Conditions", href: "#" },
+  { name: "Privacy", href: "#" },
+];
+
 export default async function Header() {
   const user = await currentUser();
 
   return (
     <header className="top-0 z-50 w-full border-b border-border bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 md:sticky">
       <div className="container flex h-14 max-w-screen-2xl items-center justify-between px-4 md:px-8">
-        <Link href="/" className="flex items-center justify-center gap-3">
-          {/* logo */}
-          <Image
-            src="/logo.svg"
-            alt="Logo"
-            className="h-10 w-10"
-            width={10}
-            height={10}
-          />
-          <h1 className="text-lg font-bold text-farm">Agira</h1>
-        </Link>
+        <div className="flex items-center gap-6">
+          <Link href="/" className="flex items-center justify-center gap-3">
+            {/* logo */}
+            <Image
+              src="/logo.svg"
+              alt="Logo"
+              className="h-10 w-10"
+              width={10}
+              height={10}
+            />
+            <h1 className="text-lg font-bold text-farm">Agira</h1>
+          </Link>
+          {links.map((link) => (
+            <Link
+              href={link.href}
+              key={link.name}
+              className="hidden text-sm text-muted-foreground hover:text-black lg:block"
+            >
+              {link.name}
+            </Link>
+          ))}
+        </div>
 
         <div className="flex items-center gap-2">
           <SearchInput />
 
           {!user && (
-            <div className="hidden items-center gap-2 md:flex">
+            <div className="hidden items-center gap-2 sm:flex">
               <Link href="/sign-in">
                 <Button variant="outline" size="sm">
                   Login
