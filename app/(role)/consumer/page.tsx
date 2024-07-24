7f09c3a57dd229ae15555a226df8394c0737bdf3
krlsdgzmn <johnkarlos.deguzman.d@gmail.com>
2024-07-24 10:50:34 +0800

    + Added consumer page

---
+++ app/(role)/consumer/page.tsx
@@ -0,0 +1,12 @@
+import Container from "@/components/container";
+import WholeSaleSection from "./_components/whole-sale-section";
+
+export default function ConsumerPage() {
+  return (
+    <Container>
+      <main>
+        <WholeSaleSection />
+      </main>
+    </Container>
+  );
+}
