a48bb27d9804dc5373f8f38c05a34a7967e653de
krlsdgzmn <johnkarlos.deguzman.d@gmail.com>
2024-07-24 18:55:21 +0800

    🔧 Changed navbar transparency

--- components/header.tsx
+++ components/header.tsx
@@ -17,7 +17,7 @@ export default async function Header() {
   const user = await currentUser();
 
   return (
-    <header className="top-0 z-50 w-full border-b border-border bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 md:sticky">
+    <header className="top-0 z-50 w-full border-b border-border bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/90 md:sticky">
       <div className="container flex h-14 max-w-screen-2xl items-center justify-between px-4 md:px-8">
         <div className="flex items-center gap-6">
           <Link href="/" className="flex items-center justify-center gap-3">
