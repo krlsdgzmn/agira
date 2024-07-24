69a6d90ac450525687f75f745fdfeeaeebdcbb12
krlsdgzmn <johnkarlos.deguzman.d@gmail.com>
2024-07-24 17:58:57 +0800

    + Added marketplace page

---
+++ app/(role)/marketplace/_components/whole-sale-section.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import ProductCard from "@/components/product-card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselPrevious,
+  CarouselNext,
+} from "@/components/ui/carousel";
+import Autoplay from "embla-carousel-autoplay";
+
+export default function MarketplaceSection({
+  title,
+  description,
+  delayCount,
+  products,
+}: {
+  title: string;
+  description: string;
+  delayCount: number;
+  products: {
+    id: number;
+    product_name: string;
+    price: number;
+    unit: string;
+    farm_name: string;
+    image: string;
+  }[];
+}) {
+  return (
+    <section className="pt-8">
+      <h1 className="text-lg font-bold md:text-xl">{title}</h1>
+      <p className="text-muted-foreground">{description}</p>
+
+      <Carousel
+        className="mx-auto w-full"
+        opts={{ loop: true }}
+        plugins={[Autoplay({ delay: delayCount })]}
+      >
+        <CarouselContent className="gap-4 p-4">
+          {products.map((item) => (
+            <CarouselItem
+              key={item.id}
+              className="max-w-[200px] basis-1/2 overflow-hidden rounded-lg border border-border bg-card pl-0 shadow sm:basis-1/3"
+            >
+              <ProductCard
+                name={item.product_name}
+                price={item.price}
+                unit={item.unit}
+                farm={item.farm_name}
+                image={item.image}
+              />
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious className="absolute left-[-1rem] top-1/2 z-10 -translate-y-1/2 transform" />
+        <CarouselNext className="absolute right-[-1rem] top-1/2 z-10 -translate-y-1/2 transform" />
+      </Carousel>
+    </section>
+  );
+}
---
+++ app/(role)/marketplace/page.tsx
@@ -0,0 +1,157 @@
+import Container from "@/components/container";
+import MarketplaceSection from "./_components/whole-sale-section";
+import Image from "next/image";
+
+const products = [
+  {
+    id: 1,
+    product_name: "Sampalok",
+    price: 115,
+    unit: "kg",
+    farm_name: "Harvest Haven Farm",
+    image: "/products/sampalok.png",
+  },
+  {
+    id: 2,
+    product_name: "Corn",
+    price: 250,
+    unit: "kg",
+    farm_name: "Kamalig Farm",
+    image: "/products/corn.png",
+  },
+  {
+    id: 3,
+    product_name: "Eggplant",
+    price: 120,
+    unit: "kg",
+    farm_name: "Conchie Farm",
+    image: "/products/eggplant.png",
+  },
+  {
+    id: 4,
+    product_name: "Cabbage",
+    price: 180,
+    unit: "kg",
+    farm_name: "ForThee Farm",
+    image: "/products/cabbage.png",
+  },
+  {
+    id: 5,
+    product_name: "Mushrooms",
+    price: 110,
+    unit: "kg",
+    farm_name: "Dinakdakan Farm",
+    image: "/products/mushrooms.png",
+  },
+  {
+    id: 6,
+    product_name: "Bell Peppers",
+    price: 220,
+    unit: "kg",
+    farm_name: "Baliwag Farm",
+    image: "/products/bell-peppers.png",
+  },
+  {
+    id: 7,
+    product_name: "Cucumbers",
+    price: 120,
+    unit: "kg",
+    farm_name: "Sinawali Farm",
+    image: "/products/cucumbers.png",
+  },
+  {
+    id: 8,
+    product_name: "Tomatoes",
+    price: 100,
+    unit: "kg",
+    farm_name: "Pinked Farm",
+    image: "/products/tomatoes.png",
+  },
+  {
+    id: 9,
+    product_name: "Strawberries",
+    price: 325,
+    unit: "kg",
+    farm_name: "Baguio Farm",
+    image: "/products/strawberries.png",
+  },
+];
+
+export default function MarketplacePage() {
+  return (
+    <Container>
+      <main className="py-8">
+        {/* Banners */}
+        <section className="grid gap-4 xl:grid-cols-5">
+          <div className="relative overflow-hidden border xl:col-span-3 xl:row-span-2 xl:h-72">
+            <Image
+              src="/agira-marketplace.png"
+              alt="Agira Marketplace"
+              className="h-full max-h-72 w-full"
+              width={300}
+              height={800}
+              quality={100}
+              unoptimized
+            />
+
+            <div className="absolute inset-0 flex max-w-[750px] items-center justify-start px-8">
+              <Image
+                src="/logo.svg"
+                alt="Logo"
+                className="h-20 w-20 sm:h-32 sm:w-32"
+                width={128}
+                height={128}
+                quality={100}
+                unoptimized
+              />
+
+              <h1 className="mx-auto text-2xl font-extrabold text-white sm:text-4xl md:text-5xl">
+                AGIRA MARKETPLACE
+              </h1>
+            </div>
+          </div>
+
+          <div className="hidden max-h-[8.5rem] overflow-hidden border xl:col-span-2 xl:block">
+            <Image
+              src="/agira-marketplace.png"
+              alt="Agira Marketplace"
+              className="w-full object-cover"
+              width={300}
+              height={800}
+              quality={100}
+              unoptimized
+            />
+          </div>
+
+          <div className="hidden max-h-[8.5rem] overflow-hidden border xl:col-span-2 xl:block">
+            <Image
+              src="/agira-marketplace.png"
+              alt="Agira Marketplace"
+              className="w-full object-cover"
+              width={300}
+              height={800}
+              quality={100}
+              unoptimized
+            />
+          </div>
+        </section>
+
+        {/* Whole Sale Section */}
+        <MarketplaceSection
+          title="Whole Sale"
+          description="Discover the Best Deals on Bulk Purchases"
+          delayCount={4000}
+          products={products}
+        />
+
+        {/* Amazing Deals Section */}
+        <MarketplaceSection
+          title="Amazing Deals"
+          description="Unbeatable Prices on Top-Quality Products"
+          delayCount={7000}
+          products={products}
+        />
+      </main>
+    </Container>
+  );
+}
