32e58f9f43e6ceb41032c3140a0f1392e91610be
krlsdgzmn <johnkarlos.deguzman.d@gmail.com>
2024-07-24 11:16:43 +0800

    🔧 Fixed conditional rendering of search input

--- components/search-input.tsx
+++ components/search-input.tsx
@@ -1,17 +1,17 @@
 "use client";
 
 import { SearchIcon } from "lucide-react";
+import { usePathname } from "next/navigation";
+import { useState } from "react";
 import { Button } from "./ui/button";
-import { Input } from "./ui/input";
 import {
-  CommandInput,
-  CommandList,
+  CommandDialog,
   CommandEmpty,
   CommandGroup,
+  CommandInput,
   CommandItem,
-  CommandDialog,
+  CommandList,
 } from "./ui/command";
-import { useState } from "react";
 
 export default function SearchInput() {
   const [open, setOpen] = useState(false);
@@ -19,30 +19,36 @@ export default function SearchInput() {
     setOpen((isOpen) => !isOpen);
   };
 
-  return (
-    <div className="mx-auto my-4 flex h-9 w-full items-center gap-2 overflow-hidden rounded-md border border-border bg-gray-50">
-      <Button
-        variant="ghost"
-        className="h-full w-full p-0 pl-2 pr-6"
-        onClick={handleSetOpen}
-      >
-        <SearchIcon className="pr-2 opacity-50" />
-        <span className="w-full text-left font-normal text-muted-foreground">
-          Search products...
-        </span>
-      </Button>
+  const currentPath = usePathname();
 
-      <CommandDialog open={open} onOpenChange={handleSetOpen}>
-        <CommandInput placeholder="Search products..." />
-        <CommandList>
-          <CommandEmpty>No results found.</CommandEmpty>
-          <CommandGroup heading="Suggestions">
-            <CommandItem>Vegetables</CommandItem>
-            <CommandItem>Fruits</CommandItem>
-            <CommandItem>Meats</CommandItem>
-          </CommandGroup>
-        </CommandList>
-      </CommandDialog>
-    </div>
+  return (
+    <>
+      {(currentPath === "/consumer" || currentPath === "/farmer") && (
+        <div className="mx-auto my-4 flex h-9 w-full items-center gap-2 overflow-hidden rounded-md border border-border bg-gray-50">
+          <Button
+            variant="ghost"
+            className="h-full w-full p-0 pl-2 pr-6"
+            onClick={handleSetOpen}
+          >
+            <SearchIcon className="pr-2 opacity-50" />
+            <span className="w-full text-left font-normal text-muted-foreground">
+              Search products...
+            </span>
+          </Button>
+
+          <CommandDialog open={open} onOpenChange={handleSetOpen}>
+            <CommandInput placeholder="Search products..." />
+            <CommandList>
+              <CommandEmpty>No results found.</CommandEmpty>
+              <CommandGroup heading="Suggestions">
+                <CommandItem>Vegetables</CommandItem>
+                <CommandItem>Fruits</CommandItem>
+                <CommandItem>Meats</CommandItem>
+              </CommandGroup>
+            </CommandList>
+          </CommandDialog>
+        </div>
+      )}
+    </>
   );
 }
