fff9dac0e775e73204e39dd8621d2d947648c9ff
krlsdgzmn <johnkarlos.deguzman.d@gmail.com>
2024-07-24 10:41:48 +0800

    + Added search bard component

---
+++ components/search-input.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { SearchIcon } from "lucide-react";
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+import {
+  CommandInput,
+  CommandList,
+  CommandEmpty,
+  CommandGroup,
+  CommandItem,
+  CommandDialog,
+} from "./ui/command";
+import { useState } from "react";
+
+export default function SearchInput() {
+  const [open, setOpen] = useState(false);
+  const handleSetOpen = (): void => {
+    setOpen((isOpen) => !isOpen);
+  };
+
+  return (
+    <div className="mx-auto my-4 flex h-9 w-full items-center gap-2 overflow-hidden rounded-md border border-border bg-gray-50">
+      <Button
+        variant="ghost"
+        className="h-full w-full p-0 pl-2 pr-6"
+        onClick={handleSetOpen}
+      >
+        <SearchIcon className="pr-2 opacity-50" />
+        <span className="w-full text-left font-normal text-muted-foreground">
+          Search products...
+        </span>
+      </Button>
+
+      <CommandDialog open={open} onOpenChange={handleSetOpen}>
+        <CommandInput placeholder="Search products..." />
+        <CommandList>
+          <CommandEmpty>No results found.</CommandEmpty>
+          <CommandGroup heading="Suggestions">
+            <CommandItem>Vegetables</CommandItem>
+            <CommandItem>Fruits</CommandItem>
+            <CommandItem>Meats</CommandItem>
+          </CommandGroup>
+        </CommandList>
+      </CommandDialog>
+    </div>
+  );
+}
